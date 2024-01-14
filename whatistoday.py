69f2bee3a281c91a7078e3c55fba7ee5a9f71e93
harshvardhankaushik7755 <harshvardhankaushik74917@gmail.com>
2024-01-14 19:16:20 +0530

    made a readme of a new repo

--- whatistoday.py
+++ whatistoday.py
@@ -7,9 +7,22 @@ def occasionOfDay(input):
 if __name__ == "__main__":
     with open("occasion.json", "r") as jsonfile:
         data = json.load(jsonfile)
+        keylist = list(data.keys())
+        # print(keylist)
     # print(data)
     userinput = None
     while userinput!="exit":
         userdate = input()
-        occasionOfDay(data[userdate])
+        checkey = False
+        for key in keylist:
+            if key == userdate:
+                occasionOfDay(data[userdate])
+                checkey = True
+            else:
+                pass
+            
+        if userdate=="exit":
+            break
+        elif (checkey == False):
+            print("Incorrect try again.")
         
